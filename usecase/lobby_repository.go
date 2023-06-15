7358dc5510ff728ca2b868c81d5e02474aec42ca
hytkgami <gagagatrump@gmail.com>
2023-06-15 17:09:29 +0900

    add usecase interfaces

--- usecase/lobby_repository.go
+++ usecase/lobby_repository.go
@@ -10,4 +10,9 @@ type LobbyRepository interface {
 	CreateLobby(ctx context.Context, uid, name string, public bool) (*domain.Lobby, error)
 	FetchLobby(ctx context.Context, id string) (*domain.Lobby, error)
 	FetchLobbies(ctx context.Context, pagination *CursorPagination) ([]*domain.Lobby, error)
+	CreateLobbyStatus(ctx context.Context, id string) error
+	FetchLobbyStatus(ctx context.Context, id string) (domain.LobbyStatus, error)
+	SubscribeLobbyStatus(ctx context.Context, id string, ch chan<- domain.LobbyStatus)
+	PublishLobbyStatus(ctx context.Context, id string, status domain.LobbyStatus) error
+	UpdateLobbyStatus(ctx context.Context, id string, status domain.LobbyStatus) error
 }
